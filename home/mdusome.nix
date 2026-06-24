129691d00fe5f5362254be119a20d54820180368
Mathew-D <mathew_dusome@kprdsb.ca>
2026-06-24 11:25:14 -0400

    theme work

--- home/mdusome.nix
+++ home/mdusome.nix
@@ -10,9 +10,7 @@
   xdg.configFile = {
     "foot/foot.ini".text = builtins.readFile ../modules/skel/foot/foot.ini;
     "foot/dank-colors.ini".text = builtins.readFile ../modules/skel/foot/dank-colors.ini;
-    "foot/themes/noctalia".text = builtins.readFile ../modules/skel/foot/themes/noctalia;
     "niri/config.kdl".text = builtins.readFile ../modules/skel/niri/config.kdl;
-    "niri/noctalia.kdl".text = builtins.readFile ../modules/skel/niri/noctalia.kdl;
     "niri/src/animations.kdl".text = builtins.readFile ../modules/skel/niri/src/animations.kdl;
     "niri/src/binds.kdl".text = builtins.readFile ../modules/skel/niri/src/binds.kdl;
     "niri/src/input.kdl".text = builtins.readFile ../modules/skel/niri/src/input.kdl;
@@ -23,7 +21,6 @@
     "noctalia/noctalia-config.toml".text = builtins.readFile ../modules/skel/noctalia/noctalia-config.toml;
     "gtk-3.0/bookmarks".text = builtins.readFile ../modules/skel/gtk-3.0/bookmarks;
     "gtk-3.0/gtk.css".text = builtins.readFile ../modules/skel/gtk-3.0/gtk.css;
-    "gtk-3.0/noctalia.css".text = builtins.readFile ../modules/skel/gtk-3.0/noctalia.css;
     "gtk-3.0/settings.ini".text = builtins.readFile ../modules/skel/gtk-3.0/settings.ini;
   };
 }
