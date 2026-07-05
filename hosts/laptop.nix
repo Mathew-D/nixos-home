794cc811c1163d724c9a3c6a25c7c26a5147414a
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-05 18:58:27 -0400

    laptop firmware location

--- hosts/laptop.nix
+++ hosts/laptop.nix
@@ -1,4 +1,14 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
+let
+  localIshFirmware = "/etc/nixos/local-firmware/ishS_MEU_aligned.bin";
+  hasLocalIshFirmware = builtins.pathExists localIshFirmware;
+
+  ish-fw = if hasLocalIshFirmware then pkgs.runCommand "intel-ish-firmware" {} ''
+    install -Dm644 "${localIshFirmware}" \
+      $out/lib/firmware/intel/ish/ish_lnlm.bin
+  '' else null;
+in
 
 {
   imports = [
@@ -13,6 +23,12 @@
   services.thermald.enable = true;
 
   hardware.enableAllFirmware = true;
+
+  warnings = lib.optional (!hasLocalIshFirmware) ''
+    Intel ISH firmware not found at ${localIshFirmware}. Place the blob there to enable it on laptop.
+  '';
+
+  hardware.firmware = lib.optional hasLocalIshFirmware ish-fw;
 
   environment.systemPackages = with pkgs; [
     iio-sensor-proxy
