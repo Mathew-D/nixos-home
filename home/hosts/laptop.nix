8c22f6be41094fa67c4bf32990b72fb4b56592c6
Mathew-D <mathew_dusome@kprdsb.ca>
2026-07-05 19:06:35 -0400

    laptop niri setup

--- home/hosts/laptop.nix
+++ home/hosts/laptop.nix
@@ -1,4 +1,8 @@
-{ ... }:
+{ lib, ... }:
 {
-  # Host-specific Home Manager options for laptop go here.
+  xdg.configFile = {
+    "niri/config.kdl".text = lib.mkForce (builtins.readFile ../skel/niri/config-laptop.kdl);
+    "niri/src/binds.kdl".text = lib.mkForce (builtins.readFile ../skel/niri/src/binds-laptop.kdl);
+    "niri/src/rules.kdl".text = lib.mkForce (builtins.readFile ../skel/niri/src/rules-laptop.kdl);
+  };
 }
