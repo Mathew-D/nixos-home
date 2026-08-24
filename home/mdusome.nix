b504c9573bce2e3af693eb862b8f843596acd6b7
Mathew-D <mathew_dusome@kprdsb.ca>
2026-08-23 23:35:31 -0400

    foot config update

--- home/mdusome.nix
+++ home/mdusome.nix
@@ -172,7 +172,43 @@ in {
     style.name = "adwaita";
   };
 
-programs.mpv = {
+programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        term = "foot";
+        "resize-delay-ms" = 50;
+        font = "RobotoMono Nerd Font Mono:size=12";
+        "selection-target" = "clipboard";
+        include = "~/.config/foot/themes/noctalia";
+        shell = "/run/current-system/sw/bin/fish";
+        pad = "10x20";
+      };
+      csd = {
+        preferred = "none";
+        size = 24;
+      };
+      scrollback = {
+        lines = 10000;
+      };
+      "colors-dark" = {
+        alpha = "0.9";
+      };
+      "mouse-bindings" = {
+        "primary-paste" = "none";
+        "select-begin-block" = "none";
+        "select-extend-character-wise" = "none";
+        "clipboard-copy" = "Control+BTN_LEFT";
+        "clipboard-paste" = "Control+BTN_RIGHT";
+      };
+      "key-bindings" = {
+        "clipboard-copy" = "Control+Shift+c";
+        "clipboard-paste" = "Control+Shift+v";
+      };
+    };
+  };
+
+  programs.mpv = {
   enable = true;
   scripts = [
     pkgs.mpvScripts.mpris
@@ -182,8 +218,6 @@ programs.mpv = {
 
   # XDG configuration files (converted from skel)
   xdg.configFile = {
-    "foot/foot.ini".text = builtins.readFile ./skel/foot/foot.ini;
-    "foot/dank-colors.ini".text = builtins.readFile ./skel/foot/dank-colors.ini;
     "niri/src/animations.kdl".text = builtins.readFile ./skel/niri/src/animations.kdl;
     "niri/machines/forest.kdl".text = builtins.readFile ./skel/niri/machines/forest.kdl;
     "niri/machines/main.kdl".text = builtins.readFile ./skel/niri/machines/main.kdl;
--- modules/skel.nix
+++ modules/skel.nix
@@ -2,8 +2,7 @@
 
 {
   environment.etc = {
-    "skel/.config/foot/foot.ini".text = builtins.readFile ../home/skel/foot/foot.ini;
-    "skel/.config/foot/dank-colors.ini".text = builtins.readFile ../home/skel/foot/dank-colors.ini;
+   # "skel/.config/foot/foot.ini".text = builtins.readFile ../home/skel/foot/foot.ini;
     "skel/.config/foot/themes/noctalia".text = builtins.readFile ../home/skel/foot/themes/noctalia;
     "skel/.config/niri/config.kdl".text = builtins.readFile ../home/skel/niri/config.kdl;
     "skel/.config/niri/noctalia.kdl".text = builtins.readFile ../home/skel/niri/noctalia.kdl;
@@ -15,9 +14,9 @@
     "skel/.config/niri/src/rules.kdl".text = builtins.readFile ../home/skel/niri/src/rules.kdl;
     "skel/.config/niri/src/spawn.kdl".text = builtins.readFile ../home/skel/niri/src/spawn.kdl;
     "skel/.config/noctalia/noctalia-config.toml".text = builtins.readFile ../home/skel/noctalia/noctalia-config.toml;
-    "skel/.config/gtk-3.0/bookmarks".text = builtins.readFile ../home/skel/gtk-3.0/bookmarks;
-    "skel/.config/gtk-3.0/gtk.css".text = builtins.readFile ../home/skel/gtk-3.0/gtk.css;
+  #  "skel/.config/gtk-3.0/bookmarks".text = builtins.readFile ../home/skel/gtk-3.0/bookmarks;
+  #  "skel/.config/gtk-3.0/gtk.css".text = builtins.readFile ../home/skel/gtk-3.0/gtk.css;
     "skel/.config/gtk-3.0/noctalia.css".text = builtins.readFile ../home/skel/gtk-3.0/noctalia.css;
-    "skel/.config/gtk-3.0/settings.ini".text = builtins.readFile ../home/skel/gtk-3.0/settings.ini;
+  #  "skel/.config/gtk-3.0/settings.ini".text = builtins.readFile ../home/skel/gtk-3.0/settings.ini;
   };
 }
